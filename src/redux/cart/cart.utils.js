b085937b000faf7161fbc64546dbb0d79b07f382
vincent <vincent.pham@squareonesoft.com>
2021-02-16 23:10:28 +0700

    add remove cartItem at checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -5,3 +5,13 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         }
         return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+        console.log('REMOVEEEE');
+        const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+        if (existingCartItem.quantity === 1) {
+                return cartItems.filter(x => x.id !== cartItemToRemove.id);
+        }
+        return cartItems.map(x => x.id === cartItemToRemove.id ? {
+                ...x, quantity: x.quantity - 1
+        } : x);
+}
